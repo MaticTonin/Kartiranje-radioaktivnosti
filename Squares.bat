6e15d9cde1228c62f5f197d42a04fdfad530c940
MaticTonin <matic.tonin@student.fmf.uni-lj.si>
2022-09-09 13:00:37 +0200

    Update

--- Squares.bat
+++
@@ -1,1 +0,0 @@
-qgis --nologo --project C:\IJS\Obdelava\Final\Kartiranje-radioaktivnosti\Map_of_radioactivity-Slovenia.qgz --code C:\IJS\Obdelava\Final\Kartiranje-radioaktivnosti\Full_version-squares.PY
