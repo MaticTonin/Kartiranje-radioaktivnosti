c2edb1968ea7c335c8372e9023cce048fdd5a7cd
MaticTonin <matic.tonin@student.fmf.uni-lj.si>
2022-09-12 12:12:04 +0200

    Update

--- Linux/Squares.bat
+++ Linux/Squares.bat
@@ -1,1 +1,3 @@
-qgis --nologo --project C:\IJS\Obdelava\Final\Kartiranje-radioaktivnosti\Linux/Map_of_radioactivity-Slovenia.qgz --code C:\IJS\Obdelava\Final\Kartiranje-radioaktivnosti\Linux/Full_version-squares.PY
+pip install pandas 
+pip install mycolorpy 
+qgis --nologo --project C:\IJS\Obdelava\Final\Kartiranje-radioaktivnosti\Linux/Map_of_radioactivity-Slovenia.qgz --code C:\IJS\Obdelava\Final\Kartiranje-radioaktivnosti\Linux/Full_version-squares.py
