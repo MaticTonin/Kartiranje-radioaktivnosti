7db403bc1a685b51c0b0df33436676d69a9f960f
MaticTonin <matic.tonin@student.fmf.uni-lj.si>
2022-09-12 12:59:43 +0200

    Update

--- Linux/Grid.bat
+++
@@ -1,3 +0,0 @@
-pip install pandas 
-pip install mycolorpy 
-qgis --nologo --project C:\IJS\Obdelava\Final\Kartiranje-radioaktivnosti\Linux/Map_of_radioactivity-Slovenia.qgz --code C:\IJS\Obdelava\Final\Kartiranje-radioaktivnosti\Linux/Full_version-grid.py
